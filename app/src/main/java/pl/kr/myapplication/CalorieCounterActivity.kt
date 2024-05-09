81d73cf24298283b75d4641d9869b5f2cf584c71
Kacper Ruta <kacperruta644@gmail.com>
2024-05-09 17:38:02 +0200

    project backup

--- app/src/main/java/pl/kr/myapplication/CalorieCounterActivity.kt
+++ app/src/main/java/pl/kr/myapplication/CalorieCounterActivity.kt
@@ -11,10 +11,18 @@ class CalorieCounterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCalorieCounterBinding
     private val configVm by viewModels<ConfigurationViewModel>()
 
-    var calories: Int = 0
-    var protein: Int = 0
-    var fats: Int = 0
-    var carbs: Int = 0
+    var calories_goal: Int = 0
+    var protein_goal: Int = 0
+    var fats_goal: Int = 0
+    var carbs_goal: Int = 0
+
+    var current_calories: Int = 0
+    var current_protein: Int = 0
+    var current_fats: Int = 0
+    var current_carbs: Int = 0
+
+
+    var points: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +30,16 @@ class CalorieCounterActivity : AppCompatActivity() {
 
         configVm.startingConfig()
 
-        calories = intent.getIntExtra("Calories",0)
-        protein = intent.getIntExtra("Protein",0)
-        fats = intent.getIntExtra("Fats",0)
-        carbs = intent.getIntExtra("Carbs",0)
+        calories_goal = intent.getIntExtra("Calories",0)
+        protein_goal = intent.getIntExtra("Protein",0)
+        fats_goal = intent.getIntExtra("Fats",0)
+        carbs_goal = intent.getIntExtra("Carbs",0)
 
+        binding.ShowCalories.text = "Calories:    " + current_calories.toString() + "  /  " + calories_goal.toString()
+        binding.ShowProtein.text = current_protein.toString() + " / " + protein_goal.toString()
+        binding.ShowFats.text = current_fats.toString() + " / " + fats_goal.toString()
+        binding.ShowCarbs.text = current_fats.toString() + " / " + carbs_goal.toString()
+        binding.ShowPoints.text = points.toString()
 
         setContentView(binding.root)
     }
---
+++ app/src/main/java/pl/kr/myapplication/MealViewModel.kt
@@ -0,0 +1,4 @@
+package pl.kr.myapplication
+
+class MealViewModel {
+}
